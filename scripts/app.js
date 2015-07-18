5e389b420658b413d589321975a9277e15e071f2
kprakobkit <kprakobkit@gmail.com>
2015-07-18 00:28:09 -0700

    Remove service code into service.js

--- scripts/app.js
+++ scripts/app.js
@@ -1,28 +1,11 @@
-define(['angular',  'controllers/controller', 'angular-route'], function(angular, controller) {
+define(['angular',  'controllers/controller', 'services/drink_service', 'angular-route'], function(angular, controller, drinkService) {
   var app = angular.module('drinkApp', ['ngRoute']);
 
   app.init = function() {
     angular.bootstrap(document, ['drinkApp']);
   }
 
-  app.service('drinkService', ['$http', function($http) {
-    function getDrinks(ingredient) {
-      return $http.get('json/' + ingredient + '.json').then(function(response) {
-        return response.data.result;
-      });
-    }
-
-    function getIngredients() {
-      return $http.get('json/ingredients.json').then(function(response) {
-        return response.data.result;
-      });
-    }
-
-    return {
-      getDrinks: getDrinks,
-      getIngredients: getIngredients
-    }
-  }]);
+  app.service('drinkService', drinkService);
 
   app.controller('mainController', controller);
 
---
+++ scripts/services/drink_service.js
@@ -0,0 +1,23 @@
+define([], function() {
+  var drinkService = function($http) {
+    function getDrinks(ingredient) {
+      return $http.get('json/' + ingredient + '.json').then(function(response) {
+        return response.data.result;
+      });
+    }
+
+    function getIngredients() {
+      return $http.get('json/ingredients.json').then(function(response) {
+        return response.data.result;
+      });
+    }
+
+    return {
+      getDrinks: getDrinks,
+      getIngredients: getIngredients
+    }
+  }
+
+  drinkService.$inject = ['$http'];
+  return drinkService;
+});
