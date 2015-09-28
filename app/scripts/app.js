a1deed651e508998596ae22cce415a61118ede8a
kprakobkit <kprakobkit@gmail.com>
2015-09-27 21:54:09 -0700

    Add google analytics.
    Signed-off-by: kprakobkit <kprakobkit@gmail.com>

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -29,6 +29,14 @@ define([
   app.directive('errSrc', errSrcDirective);
   app.directive('modalDialog', modalDialogDirective);
   app.config(routes);
+  app.run(['$rootScope', '$location', '$window', function($rootScope, $location, $window){
+    $rootScope
+    .$on('$routeChangeSuccess',
+         function(event){
+           if (!$window.ga) return;
+           $window.ga('send', 'pageview', { page: $location.path() });
+         });
+  }]);
 
   app.init = function() {
     angular.bootstrap(document, ['drinkApp']);
